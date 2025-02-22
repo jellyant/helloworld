2a77a1e66c6adfdc7608fd7761789862b4a3b3da
jellyant <jellyant@whelve.eu>
2025-02-22 20:32:24 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,7 +1,17 @@
 //-- Fichero setbit.v
-module setbit(output A);
-wire A;
-    
-assign A = 1;
+module tt_um_led_jellyant (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+    // List all unused inputs to prevent warnings
+    wire _unused = &{ena, clk, rst_n, 1'b0};
+    assign uio_out[0] = 1;
     
 endmodule

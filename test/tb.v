884d920fd66b39ed139c08dddf69ebd99af84efe
jellyant <jellyant@whelve.eu>
2025-02-24 22:23:41 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -22,8 +22,6 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-  wire [25:0] data;             // USER
-  reg [25:0] counter_check = 0; //USER
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
@@ -45,8 +43,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n),    // not reset
-      .data(data)         // USER
+      .rst_n  (rst_n)    // not reset
   );
 
 endmodule

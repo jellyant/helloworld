551dd3352f63046accd41ed849fb008698181b7e
jellyant <jellyant@whelve.eu>
2025-02-24 22:25:53 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import FallingEdge, Timer
 
 @cocotb.test()
 async def test_project(dut):

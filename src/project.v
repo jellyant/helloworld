1719cbd86389cdf9929455b020cece1f6dbf7cb8
jellyant <jellyant@whelve.eu>
2025-02-24 22:16:40 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,8 +7,8 @@ module tt_um_led_jellyant (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-    output [25:0] data);
+    input  wire       rst_n,     // reset_n - low to reset
+    output [25:0] data
 );
 
     // All output pins must be assigned. If not used, assign to 0.

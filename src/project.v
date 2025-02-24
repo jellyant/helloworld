4a8b9e0596b5cc6720fe88463505984c97e33168
jellyant <jellyant@whelve.eu>
2025-02-24 21:28:27 +0100

    4 Contador de 26 bits  Update project.v

--- src/project.v
+++ src/project.v
@@ -16,11 +16,14 @@ module tt_um_led_jellyant (
     assign uio_oe  = 0;
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, clk, rst_n, 1'b0};
-    wire A;
-    wire B;
-    assign A = ui_in[0];
-    assign B = ~A;
-    assign uo_out[0] = B;
+    wire _unused = &{ena, rst_n, 1'b0};
+    
+    //-- La salida es un registro de 26 bits, inicializado a 0
+    reg [25:0] data = 0;
+    
+    //-- Sensible al flanco de subida
+    always @(posedge clk) begin
+    //-- Incrementar el registro
+    data <= data + 1;
 
 endmodule
